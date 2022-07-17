a9fab3d3d6d8eac9e8d683d98da80b8874f007ff
CHAN,KELVIN <kechan@gmail.com>
2022-07-17 11:56:31 -0400

    data_loader test

--- realestate_nlp/common/class_extensions.py
+++ realestate_nlp/common/class_extensions.py
@@ -3,3 +3,7 @@ from functools import partialmethod
 pd.DataFrame.q_py = partialmethod(pd.DataFrame.query, engine='python')
 pd.DataFrame.defrag_index = partialmethod(pd.DataFrame.reset_index, drop=True, inplace=True)
 
+from pathlib import Path
+Path.ls = lambda x: list(x.iterdir())
+Path.lf = lambda pth, pat='*': list(pth.glob(pat))
+Path.rlf = lambda pth, pat='*': list(pth.rglob(pat))
